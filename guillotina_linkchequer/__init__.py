549908ae911475c1902e2bd6ef6c3580722d1eff
Jordi Collell <jordic@gmail.com>
2018-03-29 07:32:54 +0200

    Removed logger config, moved to guillotina main config

--- guillotina_linkchequer/__init__.py
+++ guillotina_linkchequer/__init__.py
@@ -9,15 +9,6 @@ app_settings = {
 }
 
 
-root = logging.getLogger()
-root.setLevel(logging.DEBUG)
-ch = logging.StreamHandler(sys.stdout)
-ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-ch.setFormatter(formatter)
-root.addHandler(ch)
-
-
 def includeme(root):
     """
     custom application initialization here
